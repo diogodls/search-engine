fd2fe02eec903bd01e76ed331c283ca8ee098203
didi <diogoddlsr@gmail.com>
2026-03-13 21:58:12 -0300

    deps: adiciona cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import {DocumentModule} from "./document/document.module";
 import {ConfigModule} from "@nestjs/config";
+import {CacheInterceptor, CacheModule} from "@nestjs/cache-manager";
+import {APP_INTERCEPTOR} from "@nestjs/core";
 
 @Module({
   imports: [
+    CacheModule.register({
+      ttl: 0,
+      max: 1000,
+      isGlobal: true,
+    }),
     ConfigModule.forRoot({isGlobal: true}),
-    DocumentModule
+    DocumentModule,
+  ],
+  providers: [
+    {provide: APP_INTERCEPTOR, useClass: CacheInterceptor}
   ]
 })
 export class AppModule {}
