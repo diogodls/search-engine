de85775aa118dce2063df4e9b7212df7fd7f35a2
didi <diogoddlsr@gmail.com>
2026-03-30 22:02:32 -0300

    feat: use validation in dtos

--- src/document/dto/document.dto.ts
+++ src/document/dto/document.dto.ts
@@ -1,6 +1,15 @@
+import {IsNumber, IsOptional, IsString} from "class-validator";
+
 export class DocumentDto {
-  id: number;
+  @IsNumber()
+  @IsOptional()
+  id?: number;
+
+  @IsString({message: 'The title must be string type.'})
   title: string;
+
+  @IsString({message: 'The article must be string type.'})
   article: string;
+
   documentLength?: number;
 }
